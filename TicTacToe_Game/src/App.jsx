12ad111f4f4ebffc17b7c5431bdc88881fde2e0c
srinidhi <srinidhisomayaji9113@gmail.com>
2023-08-31 22:58:42 +0530

    Error fixed - Project Completed!!!

--- TicTacToe_Game/src/App.jsx
+++ TicTacToe_Game/src/App.jsx
@@ -7,58 +7,59 @@ import PopUp from './PopUp'
 const socket = io.connect('http://localhost:3000')
 
 export default function App() {
-    const [player,setPlayer] = useState('o')
-    const [board,setBoard] = useState([['','',''],['','',''],['','','']])
+    const player = useRef('x')
+    const board = useRef([['','',''],['','',''],['','','']])
     const [winner,setWinner] = useState(false)
     const [room,setRoom] = useState("")
-    const turnRef = useRef(false)
+    const [turn,setTurn] = useState(false)
 
     useEffect(()=>{
       socket.on("turn",(data)=>{
-        turnRef.current = data
+        setTurn(data)
         const popUp = document.getElementById("pop-up")
         popUp.style.transform = "scale(0)"
       })
       socket.on("error",(data)=>{
 
       })
+    },[])
+
+    useEffect(()=>{
       socket.on("recv-move",(data)=>{
-        console.log("Player:"+player,"turn:"+turnRef.current)
         const tile = document.getElementById(data)
         const tile_back = tile.querySelector(".tile-back")
         tile.classList.add("clicked")
         const [k1,k2] = data
-        let newBoard = board
+        let newBoard = board.current
         if(newBoard[parseInt(k1)-1][parseInt(k2)-1]==''){
-          tile_back.innerHTML = player
-          tile_back.style.backgroundColor = player==='x'?'#f83e4b':'dodgerblue'
-          newBoard[parseInt(k1)-1][parseInt(k2)-1] = player
-          setBoard(newBoard)
-          setPlayer((player)=> player==='x'?'o':'x')
+          tile_back.innerHTML = player.current
+          tile_back.style.backgroundColor = player.current==='x'?'#f83e4b':'dodgerblue'
+          newBoard[parseInt(k1)-1][parseInt(k2)-1] = player.current
+          board.current = newBoard
+          player.current = player.current=='x'?'o':'x'
         }
-        const win = GameLogic(board)
+        const win = GameLogic(board.current)
         if(win) setWinner(win)
       })
-    },[])
+    },[turn])
 
     const playerClick = (event) => {
-      console.log("Player:"+player,"turn:"+board)
-      if(!winner && player===turnRef.current){
+      if(!winner && player.current===turn){
         const tile = event.currentTarget
         const tile_back = document.getElementById(tile.id).querySelector(".tile-back")
         tile.classList.add("clicked")
         const [k1,k2] = tile.id
-        let newBoard = board
+        let newBoard = board.current
         if(newBoard[parseInt(k1)-1][parseInt(k2)-1]==''){
-          tile_back.innerHTML = player
-          tile_back.style.backgroundColor = player==='x'?'#f83e4b':'dodgerblue'
-          newBoard[parseInt(k1)-1][parseInt(k2)-1] = player          
-          socket.emit("send-move",{room:room,move:tile.id})
-          setPlayer((player)=> player==='x'?'o':'x')
-          setBoard(newBoard)
+          tile_back.innerHTML = player.current
+          tile_back.style.backgroundColor = player.current==='x'?'#f83e4b':'dodgerblue'
+          newBoard[parseInt(k1)-1][parseInt(k2)-1] = player.current
+          board.current = newBoard
+          player.current = player.current=='x'?'o':'x'
         }
-        const win = GameLogic(board)
+        const win = GameLogic(board.current)
         if(win) setWinner(win)
+        socket.emit("send-move",{room:room,move:tile.id})
       }
     }
 
@@ -67,9 +68,10 @@ export default function App() {
       tiles.forEach(tile => {
         tile.classList.remove("clicked")
       })
-      setPlayer(player=>player==='x'?'o':'x')
+      player.current = 'x'
       setWinner(false)
-      setBoard([['','',''],['','',''],['','','']])
+      board.current = [['','',''],['','',''],['','','']]
+      setTurn((turn)=>turn==='x'?'o':'x')
     }
 
     const roomValue = (event) => {
@@ -86,7 +88,7 @@ export default function App() {
       <div className='title'>
         <h1>TicTacToe</h1>
       </div>
-      {turnRef && <h3>{turnRef.current=='x'?"You Start":"Opponent Starts"}</h3>}
+      {turn && <h3>{turn==='x'?"You Start":"Opponent Starts"}</h3>}
       <div className="play-board">
         <Tile id="11" onClick={playerClick}/>
         <Tile id="12" onClick={playerClick}/>
